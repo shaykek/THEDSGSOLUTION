3ba9f433c00a19e4e79fc5887c31f8d5990223f8
rmnbrtshk <rmnbrtshk@gmail.com>
2019-07-25 17:53:05 +0300

    Updates

--- precache-manifest.d8e2ec5dc813249eaedaa1c7018951c5.js
+++ precache-manifest.d8e2ec5dc813249eaedaa1c7018951c5.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "47163191244d8fb063b25113c75f99b8",
+    "revision": "d46e4107350a88400f76b5ef6b46c56e",
     "url": "/THEDSGSOLUTION/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/THEDSGSOLUTION/static/css/3.53154b79.chunk.css"
   },
   {
-    "revision": "9e3f6dd62a7086954c59",
+    "revision": "7f3432586ae805daf427",
     "url": "/THEDSGSOLUTION/static/css/4.fe941930.chunk.css"
   },
   {
@@ -36,8 +36,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/THEDSGSOLUTION/static/js/3.c5caaa45.chunk.js"
   },
   {
-    "revision": "9e3f6dd62a7086954c59",
-    "url": "/THEDSGSOLUTION/static/js/4.13300637.chunk.js"
+    "revision": "7f3432586ae805daf427",
+    "url": "/THEDSGSOLUTION/static/js/4.12025e4f.chunk.js"
   },
   {
     "revision": "0f7b61415270b2f1feba",
@@ -64,7 +64,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/THEDSGSOLUTION/static/js/main.26cd4282.chunk.js"
   },
   {
-    "revision": "0b0a105399f094d8cff6",
-    "url": "/THEDSGSOLUTION/static/js/runtime~main.84a4a2b9.js"
+    "revision": "c845ea0b04cbf0e8d7df",
+    "url": "/THEDSGSOLUTION/static/js/runtime~main.ba827a2f.js"
   }
 ]);
