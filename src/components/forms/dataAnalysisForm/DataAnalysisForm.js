982d590f41f5944605f2abe0fadcda70047dc39b
Roman <rmnbrtshk@gmail.com>
2019-07-25 17:52:24 +0300

    Add validation

--- src/components/forms/dataAnalysisForm/DataAnalysisForm.js
+++ src/components/forms/dataAnalysisForm/DataAnalysisForm.js
@@ -9,13 +9,37 @@ import validation from "../../../hooks/validation";
 const DataAnalysisForm = ({ dataAnalysisForm: { title, phonecodes } }) => {
   const { data } = useFetch(phonecodes);
 
+  const {
+    formState,
+    handleChange,
+    handleSubmit,
+    errors,
+    wasSubmitted,
+    setWasSubmitted
+  } = useForm(
+    {
+      phone: "",
+      name: ""
+    },
+    // put to setWasSubmitted() boolean value to show that form was send successfully or not
+    data => setWasSubmitted(true),
+    validation
+  );
+  console.log(errors);
+
   return (
     <div className="analysis">
       <form>
         <h2 className="analysis__title">{title}</h2>
         <div className="analysis__container">
           <div className="analysis__inner analysis__inner--down">
-            <input type="email" className="analysis__field" />
+            <input
+              type="text"
+              name="name"
+              className="analysis__field"
+              onChange={handleChange}
+              value={formState.name}
+            />
             <label>
               דוא"ל{" "}
               <Image
@@ -23,6 +47,7 @@ const DataAnalysisForm = ({ dataAnalysisForm: { title, phonecodes } }) => {
                 src="images/analysis/name.png"
               />
             </label>
+            {errors.name && <p className="error">{errors.name}</p>}
           </div>
           <div className="analysis__inner ">
             <div className="analysis__field analysis__field--box">
@@ -36,6 +61,9 @@ const DataAnalysisForm = ({ dataAnalysisForm: { title, phonecodes } }) => {
               <input
                 type="text"
                 className="analysis__field analysis__field--none-line analysis__phone"
+                name="phone"
+                onChange={handleChange}
+                value={formState.phone}
               />
             </div>
             <label>
@@ -45,9 +73,16 @@ const DataAnalysisForm = ({ dataAnalysisForm: { title, phonecodes } }) => {
                 src="images/analysis/tube.png"
               />
             </label>
+            {errors.phone && <p className="error">{errors.phone}</p>}
           </div>
           <div className="analysis__inner analysis__inner--down">
-            <input type="text" className="analysis__field" />
+            <input
+              type="email"
+              className="analysis__field"
+              name="email"
+              onChange={handleChange}
+              value={formState.email}
+            />
             <label>
               שם{" "}
               <Image
@@ -55,6 +90,7 @@ const DataAnalysisForm = ({ dataAnalysisForm: { title, phonecodes } }) => {
                 src="images/analysis/envelope.png"
               />
             </label>
+            {errors.email && <p className="error">{errors.email}</p>}
           </div>
         </div>
         <input className="analysis__send" type="submit" value={title} />
