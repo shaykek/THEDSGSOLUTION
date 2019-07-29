38d17029cdaf4535aefaa66784a0a1ffd5f9a61b
Roman <rmnbrtshk@gmail.com>
2019-07-29 10:16:18 +0300

    Change DataAnalysisForm

--- src/components/forms/dataAnalysisForm/DataAnalysisForm.js
+++ src/components/forms/dataAnalysisForm/DataAnalysisForm.js
@@ -19,82 +19,98 @@ const DataAnalysisForm = ({ dataAnalysisForm: { title, phonecodes } }) => {
   } = useForm(
     {
       phone: "",
-      name: ""
+      name: "",
+      email: "",
+      code: ""
     },
     // put to setWasSubmitted() boolean value to show that form was send successfully or not
     data => setWasSubmitted(true),
     validation
   );
-  console.log(errors);
 
   return (
     <div className="analysis">
-      <form>
-        <h2 className="analysis__title">{title}</h2>
-        <div className="analysis__container">
-          <div className="analysis__inner analysis__inner--down">
-            <input
-              type="text"
-              name="name"
-              className="analysis__field"
-              onChange={handleChange}
-              value={formState.name}
-            />
-            <label>
-              דוא"ל{" "}
-              <Image
-                className="analysis__image"
-                src="images/analysis/name.png"
-              />
-            </label>
-            {errors.name && <p className="error">{errors.name}</p>}
-          </div>
-          <div className="analysis__inner ">
-            <div className="analysis__field analysis__field--box">
-              <select className="analysis__field analysis__field--none-line analysis__code">
-                <option>code</option>
-                {data.map(({ dial_code }, i) => (
-                  <option key={i}>{dial_code}</option>
-                ))}
-              </select>
-
+      {wasSubmitted && (
+        <div className="leave-data-form__success">
+          הפרטים נשלחו בהצלחה, תודה
+        </div>
+      )}
+      {!wasSubmitted && (
+        <form onSubmit={handleSubmit}>
+          <h2 className="analysis__title">{title}</h2>
+          <div className="analysis__container">
+            <div className="analysis__inner analysis__inner--down">
               <input
                 type="text"
-                className="analysis__field analysis__field--none-line analysis__phone"
-                name="phone"
+                name="name"
+                className="analysis__field"
                 onChange={handleChange}
-                value={formState.phone}
+                value={formState.name}
+                id="name"
               />
+              <label htmlFor="name">
+                דוא"ל{" "}
+                <Image
+                  className="analysis__image"
+                  src="images/analysis/name.png"
+                />
+              </label>
+              {errors.name && <p className="error">{errors.name}</p>}
             </div>
-            <label>
-              מספר טלפון{" "}
-              <Image
-                className="analysis__image"
-                src="images/analysis/tube.png"
+            <div className="analysis__inner ">
+              <div className="analysis__field analysis__field--box">
+                <select
+                  onChange={handleChange}
+                  name="code"
+                  className="analysis__field analysis__field--none-line analysis__code"
+                >
+                  <option value="">code</option>
+                  {data.map(({ dial_code }, i) => (
+                    <option key={i}>{dial_code}</option>
+                  ))}
+                </select>
+
+                <input
+                  type="text"
+                  className="analysis__field analysis__field--none-line analysis__phone"
+                  name="phone"
+                  onChange={handleChange}
+                  value={formState.phone}
+                  id="phone"
+                />
+              </div>
+              <label htmlFor="phone">
+                מספר טלפון{" "}
+                <Image
+                  className="analysis__image"
+                  src="images/analysis/tube.png"
+                />
+              </label>
+              {errors.phone && <p className="error">{errors.phone}</p>}
+              {errors.code && <p className="error">{errors.code}</p>}
+            </div>
+            <div className="analysis__inner analysis__inner--down">
+              <input
+                type="email"
+                className="analysis__field"
+                name="email"
+                onChange={handleChange}
+                value={formState.email}
+                id="email"
               />
-            </label>
-            {errors.phone && <p className="error">{errors.phone}</p>}
+              <label htmlFor="email">
+                שם{" "}
+                <Image
+                  className="analysis__image"
+                  src="images/analysis/envelope.png"
+                />
+              </label>
+              {errors.email && <p className="error">{errors.email}</p>}
+            </div>
           </div>
-          <div className="analysis__inner analysis__inner--down">
-            <input
-              type="email"
-              className="analysis__field"
-              name="email"
-              onChange={handleChange}
-              value={formState.email}
-            />
-            <label>
-              שם{" "}
-              <Image
-                className="analysis__image"
-                src="images/analysis/envelope.png"
-              />
-            </label>
-            {errors.email && <p className="error">{errors.email}</p>}
-          </div>
-        </div>
-        <input className="analysis__send" type="submit" value={title} />
-      </form>
+          <input className="analysis__send" type="submit" value={title} />
+        </form>
+      )}
     </div>
   );
 };
--- src/hooks/validation.js
+++ src/hooks/validation.js
@@ -1,7 +1,7 @@
 const validation = (input, errors, name) => {
   if (name === "phone") {
     const error = !input["phone"]
-      ? "שדה נדרש"
+      ? "שדה הטלפון נדרש"
       : !/^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s./0-9]*$/.test(input[name])
       ? "שדה מספר הטלפון אינו חוקי"
       : "";
@@ -11,6 +11,24 @@ const validation = (input, errors, name) => {
 
   if (name === "name") {
     const error = !input["name"] ? "שדה נדרש" : "";
+
+    return { [name]: error };
+  }
+
+  if (name === "code") {
+    const error = !input["code"] ? "שדה קוד הטלפון נדרש" : "";
+
+    return { [name]: error };
+  }
+
+  if (name === "email") {
+    const error = !input["email"]
+      ? "שדה נדרש"
+      : !/^\s*([a-zA-Z]){2,20}[@]([a-zA-Z]){2,10}[.]([a-zA-Z]){2,5}\s*$/i.test(
+          input[name]
+        )
+      ? 'שדה הדוא"ל אינו חוקי'
+      : "";
 
     return { [name]: error };
   }
